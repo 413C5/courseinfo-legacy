fed893c830fe7bdafa3a65b14ef3606014ecee2d
413C5 <a_gonzalezzarate2@yahoo.com>
2023-02-08 18:00:37 -0600

    Paso 2
    Eliminacion de archivos innecesarios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+//Nombre del curso
+const Header=(props)=>(<div><h1>Hello {props.course}</h1></div>)
+
+const Part=(props)=>(<div><p>{props.part} {props.exercises}</p></div>)
+
+//Partes y su número de ejercicios
+const Content=(props)=>{
+  return(
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+
+//Numero total de ejercicios
+const Total=(props)=>( 
+  <div>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  </div>
+)
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  
+  return (
+    <div>
+    <Header course={course} />
+    <Content part1={part1} exercises1={exercises1}
+             part2={part2} exercises2={exercises2}
+             part3={part3} exercises3={exercises3} />
+    <Total exercises1={exercises1}
+           exercises2={exercises2}
+           exercises3={exercises3} />
+    </div>
+    )
+   
+ }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +57,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
