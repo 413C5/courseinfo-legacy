1a95d7158ed62b7e92fb21fd436adf5ab2f0ec38
413C5 <a_gonzalezzarate2@yahoo.com>
2023-02-13 22:01:09 -0600

    Parte 5
    Se cambio el curso y sus partes en un solo objeto JavaScript

--- src/index.js
+++ src/index.js
@@ -3,55 +3,62 @@ import ReactDOM from 'react-dom/client';
 
 
 //Nombre del curso
-const Header=(props)=>(<div><h1>{props.course}</h1></div>)
+const Header=(props)=>(
+  //console.log(props),
+  <div><h1>{props.course.name}</h1></div>
+)
 
 //Parte
-const Part=(props)=>(<div><p>{props.parts.name} {props.parts.exercises}</p></div>)
+const Part=(props)=>(
+  //console.log(props),
+  <div><p>{props.parts.name} {props.parts.exercises}</p></div>
+)
 
 //Partes y su número de ejercicios
 const Content=(props)=>{
   return(
+    //console.log(props),
     <div>
-      <Part parts={props.parts[0]}/>
-      <Part parts={props.parts[1]}/>
-      <Part parts={props.parts[2]}/>
-    </div>
+      <Part parts={props.parts.parts[0]}/>
+      <Part parts={props.parts.parts[1]}/>
+      <Part parts={props.parts.parts[2]}/>
+    </div> 
   )
 }
 
-
 //Numero total de ejercicios
 const Total=(props)=>( 
+  //console.log(props),
   <div>
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <p>Number of exercises {props.parts.parts[0].exercises + props.parts.parts[1].exercises + props.parts.parts[2].exercises}</p>
   </div>
 )
 
 
-
 const App = () => {
-  //const definitions
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
   return (
     <div>
         <Header course={course} />
-        <Content parts={parts} />
-        <Total parts={parts} />
+        <Content parts={course} />
+        <Total parts={course} />
     </div>
     )
    
